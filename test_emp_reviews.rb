7a5866c42b9b0b879643cc6792994e5881b0089e
kscotteng <kscott@gmail.com>
2016-02-13 10:32:29 -0500

    add test for total salary of all employees within a department

--- test_emp_reviews.rb
+++ test_emp_reviews.rb
@@ -11,8 +11,8 @@ class EmpDepTest < Minitest::Test
 
 # Create a new department (given its name).
   def test_new_department
-    a = Department.new("Engineering")
-    assert_equal a.department, "Engineering"
+    a = Department.new("Development Engineering")
+    assert_equal a.department, "Development Engineering"
   end
 
 # Create a new employee (given his/her name, email address, phone number, and salary).
@@ -27,7 +27,7 @@ class EmpDepTest < Minitest::Test
 # Add an employee to a department.
   def test_add_emp_to_dept
     a = Employee.new("John Smith", "jsmith@gmail.com", "123.456.7890", 100000)
-    b = Department.new("Engineering")
+    b = Department.new("Development Engineering")
     b.add_employee(a)
     assert_equal b.deptstaff[0].name, "John Smith"
     assert_equal b.deptstaff[0].salary, 100000
@@ -54,12 +54,38 @@ class EmpDepTest < Minitest::Test
     assert_equal b.salary, 120000
     refute_equal b.salary, 5
   end
+
+# Get a department's name.
+  def test_getdeptname
+    a = Department.new("Development Engineering")
+    b = Department.new("Process Engineering")
+    assert_equal a.department, "Development Engineering"
+    refute_equal a.department, "Process Engineering"
+    assert_equal b.department, "Process Engineering"
+    refute_equal b.department, "Development Engineering"
+  end
+
+# Get a total salary for all employees in a department.
+  def test_getdeptsalary
+    a = Department.new("Development Engineering")
+    b = Employee.new("John Smith", "jsmith@gmail.com", "123.456.7890", 100000)
+    c = Employee.new("John Doe", "jdoe@gmail.com", "123.456.7897", 120000)
+    a.add_employee(b)
+    a.add_employee(c)
+    dept_salary = a.deptstaff[0].salary + a.deptstaff[1].salary
+    assert_equal dept_salary, 220000
+    refute_equal dept_salary, 22000
+    end
+
+# Add some employee review text (a paragraph or two) to an employee.
+  def 
+
+  end
+
 end
 
 
-# Get a department's name.
-# Get a total salary for all employees in a department.
-# Add some employee review text (a paragraph or two) to an employee.
+
 # Mark whether an employee is performing satisfactorily or not satisfactorily.
 # Give a raise to an individual. You decide if this makes sense in dollars or in percent.
 # Give raises to a department's employees. You must pass this method a total dollar amount, and it must distribute the raise amounts reasonably to the department's employees. Only employees who are performing satisfactorily should get raises.
